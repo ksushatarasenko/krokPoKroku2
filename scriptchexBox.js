c257b0cbf3b8c91cb3d09e4c52da24ee1253b0db
Oksana <ksushatarasenko@gmail.com>
2024-12-28 09:43:57 +0100

    lekcja 202D1

--- scriptchexBox.js
+++ scriptchexBox.js
@@ -1,47 +1,26 @@
-// script checkBox
+// -------------- выбор несколько ответов
 
-function checkCheckbox(taskId) {
-    const correctAnswers = {
-        task506E3: {
-            question1: ['3'], 
-            question2: ['3'],     
-            question3: ['3'],    
-            question4: ['3'],    
-            question5: ['3'], 
-            question6: ['2'],    
-            question7: ['3'],   
-            question8: ['3']  
-        }
-    };
-
-    let allCorrect = true;
-
-    if (correctAnswers[taskId]) {
-        const answers = correctAnswers[taskId];
-
-        for (const [question, correctValues] of Object.entries(answers)) {
-            const selectedAnswers = document.querySelectorAll(`#${taskId} input[name="${question}"]:checked`);
-            const selectedValues = Array.from(selectedAnswers).map(input => input.value);
-            const taskElement = document.querySelector(`#${taskId} .task:has(input[name="${question}"])`);
-
-            // Проверка на совпадение выбранных значений с правильными
-            if (selectedValues.length === correctValues.length && selectedValues.every(val => correctValues.includes(val))) {
-                taskElement.classList.add('correct');
-                taskElement.classList.remove('incorrect');
-            } else {
-                taskElement.classList.add('incorrect');
-                taskElement.classList.remove('correct');
-                allCorrect = false;
-            }
-        }
-    } else {
-        console.error(`No correct answers provided for task id: ${taskId}`);
-        return;
-    }
-
-   
+function arraysEqual(arr1, arr2) {
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
 }
 
-document.getElementById('checkCheckbox').addEventListener('click', function() {
-    checkCheckbox('task506E3');
-});
+function checkAnswersLabel(formId, resultId) {
+    const form = document.getElementById(formId);
+    const questions = form.getElementsByClassName('question');
+    let score = 0;
+
+    for (let i = 0; i < questions.length; i++) {
+        const question = questions[i];
+        const correctAnswers = question.getAttribute('data-correct').split(',');
+        const selectedAnswers = Array.from(form.querySelectorAll(`input[name="q${i + 1}"]:checked`)).map(el => el.value);
+        
+        const resultElement = document.getElementById(resultId);
+
+        if (arraysEqual(selectedAnswers, correctAnswers)) {
+            score++;
+            resultElement.innerHTML = "Правильно! <img src='https://i.pinimg.com/564x/0b/5d/1f/0b5d1f62c0c6ddaa2c9c465264c5343c.jpg' alt='Correct Smiley' width='60' height='60' />";
+        } else {
+            resultElement.innerHTML = "Неправильно! <img src='https://i.pinimg.com/564x/12/7a/db/127adb0185cb2c9c8abac2f28966bb97.jpg' alt='Incorrect Smiley' width='60' height='60' />";
+        }
+    }
+}
