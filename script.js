6b0de5864333487e1d8ee972c18975d750028a02
Oksana <ksushatarasenko@gmail.com>
2025-07-17 11:31:24 +0200

    stert temat 9/6

--- script.js
+++ script.js
@@ -222,4 +222,15 @@ function initWordStrikethrough() {
 // 
 
 
+// AUDIOO
 
+document.addEventListener("DOMContentLoaded", function() {
+        document.querySelectorAll('p[data-audio] img').forEach(function(img) {
+            img.addEventListener("click", function() {
+                const p = this.closest('p[data-audio]');
+                const audioSrc = p.getAttribute("data-audio");
+                const audio = new Audio(audioSrc);
+                audio.play();
+            });
+        });
+    });
